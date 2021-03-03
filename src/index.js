61fa10d0de66cf272fd7c987e7ca40b2b03554ff
Yurchak Veronika <yurchakveronika1997@gmail.com>
2021-03-03 14:37:30 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        n = n-n*2;
+    }
+    n = String(n);
+    let arr = n.split('');
+    arr = arr.reverse();
+    arr = arr.join('')
+    n = Number(arr)
+    return n;
 }
